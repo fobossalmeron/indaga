0eb480e164b442c4a491e72eba728acad5e9bf9b
Rodrigo Salmeron <rodrigosalmeron@Minos.local>
2024-05-16 23:17:55 -0600

    test complete

--- src/app/cafe_san_francisco/page.tsx
+++ src/app/cafe_san_francisco/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function CafeSanFrancisco() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -9,10 +9,13 @@ export default function Home() {
           src="/indaga.svg"
           alt="Indaga Logo"
           className={styles.logo}
-          width={470}
-          height={150}
+          width={280}
+          height={90}
           priority
         />
+        <p className={styles.t1}>Gracias por estar aquí</p>
+        <p  className={styles.t2}>Acércate a la barra y compárteles el código:</p>
+        <p  className={styles.t3}>Golfo de México</p>
       </div>
     </main>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
+import styles from "./layout.module.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <div className={styles.footer}>
+          <a href="mailto:hello@indaga.site">Contacto</a>© Indaga, 2024
+        </div>
+      </body>
       <GoogleAnalytics gaId="G-1QH9PC856P" />
     </html>
   );
