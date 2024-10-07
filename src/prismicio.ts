4acd89e34ff8b0ed17cf45d85b4d2a9d4789ab7e
Rodrigo Salmeron <rodrigosalmeron@Rodrigos-Mac-mini.local>
2024-10-06 20:20:42 -0600

    leave api open no cache

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -7,9 +7,22 @@ export const repositoryName = config.repositoryName
 /**
  * @param config - Configuration for the Prismic client.
  */
+/**
+ * Configuración para el cliente de Prismic sin caché.
+ */
+const clientConfig: prismic.ClientConfig = {
+  fetchOptions: {
+    cache: "no-store",
+    next: { revalidate: 0 }
+  }
+};
 
+/**
+ * @param config - Configuración para el cliente de Prismic.
+ */
 export function createClient(config: prismicNext.CreateClientConfig = {}) {
   return prismic.createClient(repositoryName, {
+    ...clientConfig,
     ...config,
   });
 }
