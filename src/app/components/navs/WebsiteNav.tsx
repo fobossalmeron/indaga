2ce73db5e2e0e2b2a6f47093fd2bd124bc167b5e
Rodrigo Salmeron <fobos.salmeron@gmail.com>
2025-09-22 22:53:44 -0600

    nav ready

--- src/app/components/navs/WebsiteNav.tsx
+++ src/app/components/navs/WebsiteNav.tsx
@@ -11,16 +11,16 @@ export const WebsiteNav = () => {
   const isHomepageOrNosotras = pathname === "/" || pathname === "/nosotras";
 
   return (
-    <nav className="text-accent fixed top-0 right-0 left-0 z-50 flex h-16 w-full items-center justify-center bg-white/10 px-5 backdrop-blur-lg sm:px-10 md:px-20">
+    <nav className="text-accent fixed top-0 right-0 left-0 z-50 flex h-16 w-full items-center justify-center px-5 mix-blend-difference backdrop-blur-lg sm:px-10 md:px-20">
       <div className="flex w-full max-w-[1240px] items-center justify-between">
         <Link href="/" className="transition-all">
           <Logo
             width={95}
             height={18}
-            className={`animate-fadeSimple transition-all ${isHomepageOrNosotras ? "text-white" : "text-foreground"}`}
+            className={`animate-fadeSimple transition-all ${isHomepageOrNosotras ? "text-white" : "text-white"}`}
           />
         </Link>
-        <nav className="animate-fadeSimple flex items-center gap-7 mix-blend-exclusion transition-all">
+        <nav className="animate-fadeSimple flex items-center gap-7 transition-all">
           <ul className="flex items-center gap-3 transition-all">
             <li className="hidden lg:block">
               <NavLink href="/agenda">Agenda</NavLink>
@@ -38,19 +38,21 @@ export const WebsiteNav = () => {
         </nav>
         <div className="animate-fadeSimple flex gap-2">
           {/* Botón temporal para poder publicar */}
-          <Link
+          {/* <Link
             href="/ReporteAgosto_compressed.pdf"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Button size="thin">Descarga el reporte</Button>
-          </Link>
+            <Button size="thin" className="bg-gray-400 text-black">
+              Descarga el reporte
+            </Button>
+          </Link> */}
 
           {/* Login/registro ocultos temporalmente */}
-          {false && (
+          {true && (
             <>
               <Link href="/login">
-                <Button size="thin" variant="link">
+                <Button size="thin" variant="link" className="text-white">
                   Iniciar sesión
                 </Button>
               </Link>
